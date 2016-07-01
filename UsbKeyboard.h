c14234cd471111e9d8e695c1017f4655bf3f77d8
Zaher Dirkey <zaherdirkey@yahoo.com>
2016-07-01 19:41:09 +0300

    Add: osccal but not tested yet Fix: Delay when after init usb

--- UsbKeyboard.h
+++ UsbKeyboard.h
@@ -5,15 +5,23 @@
  */
 #ifndef __UsbKeyboard_h__
 #define __UsbKeyboard_h__
+//#define _OSCCAL_
+/*
+	Please Set USB_CFG_HAVE_MEASURE_FRAME_LENGTH to 1 in usbconfig.h
+	
+#define USB_CFG_HAVE_MEASURE_FRAME_LENGTH   1
+
+*/	
 
 #include <string.h>
 
 #include <avr/io.h>
 #include <avr/wdt.h>
-#include <avr/interrupt.h>  /* for sei() */
-#include <util/delay.h>     /* for _delay_ms() */
+#include <avr/eeprom.h>   //Eeprom read
+#include <avr/interrupt.h>  // for sei() 
+#include <util/delay.h>     // for _delay_ms() 
 
-#include <avr/pgmspace.h>   /* required by usbdrv.h */
+#include <avr/pgmspace.h>   // required by usbdrv.h 
 
 #ifdef __cplusplus
 extern "C"
@@ -407,6 +415,100 @@ const uint8_t asciimap[128] =
 	0				// DEL
 };
  
+#ifdef _OSCCAL_
+/* ------------------------------------------------------------------------- */
+/* ------------------------ Oscillator Calibration ------------------------- */
+/* ------------------------------------------------------------------------- */
+
+/* Calibrate the RC oscillator to 8.25 MHz. The core clock of 16.5 MHz is
+ * derived from the 66 MHz peripheral clock by dividing. Our timing reference
+ * is the Start Of Frame signal (a single SE0 bit) available immediately after
+ * a USB RESET. We first do a binary search for the OSCCAL value and then
+ * optimize this value with a neighboorhod search.
+ * This algorithm may also be used to calibrate the RC oscillator directly to
+ * 12 MHz (no PLL involved, can therefore be used on almost ALL AVRs), but this
+ * is wide outside the spec for the OSCCAL value and the required precision for
+ * the 12 MHz clock! Use the RC oscillator calibrated to 12 MHz for
+ * experimental purposes only!
+ */
+static void calibrateOscillator(void)
+{
+uchar       step = 128;
+uchar       trialValue = 0, optimumValue;
+int         x, optimumDev, targetValue = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
+
+    /* do a binary search: */
+    do{
+        OSCCAL = trialValue + step;
+        x = usbMeasureFrameLength();    /* proportional to current real frequency */
+        if(x < targetValue)             /* frequency still too low */
+            trialValue += step;
+        step >>= 1;
+    }while(step > 0);
+    /* We have a precision of +/- 1 for optimum OSCCAL here */
+    /* now do a neighborhood search for optimum value */
+    optimumValue = trialValue;
+    optimumDev = x; /* this is certainly far away from optimum */
+    for(OSCCAL = trialValue - 1; OSCCAL <= trialValue + 1; OSCCAL++){
+        x = usbMeasureFrameLength() - targetValue;
+        if(x < 0)
+            x = -x;
+        if(x < optimumDev){
+            optimumDev = x;
+            optimumValue = OSCCAL;
+        }
+    }
+    OSCCAL = optimumValue;
+}
+/*
+Note: This calibration algorithm may try OSCCAL values of up to 192 even if
+the optimum value is far below 192. It may therefore exceed the allowed clock
+frequency of the CPU in low voltage designs!
+You may replace this search algorithm with any other algorithm you like if
+you have additional constraints such as a maximum CPU clock.
+For version 5.x RC oscillators (those with a split range of 2x128 steps, e.g.
+ATTiny25, ATTiny45, ATTiny85), it may be useful to search for the optimum in
+both regions.
+*/
+
+void usbEventResetReady(void)
+{
+    calibrateOscillator();
+    eeprom_write_byte(0, OSCCAL);   /* store the calibrated value in EEPROM */
+}
+#endif _OSCCAL_
+
+usbMsgLen_t usbFunctionSetup(uchar data[8])
+{
+	usbRequest_t    *rq = (usbRequest_t *)((void *)data);	
+	
+	//static uchar dataBuffer[4];  /* buffer must stay valid when usbFunctionSetup returns */
+
+	if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS)
+	{
+		/* class request type */
+
+		if(rq->bRequest == USBRQ_HID_GET_REPORT)
+		{
+			/* wValue: ReportType (highbyte), ReportID (lowbyte) */
+			//usbMsgPtr = (uchar *)&UsbKeyboard.keyboard_data;
+			/* we only have one report type, so don't look at wValue */ 
+			// TODO: Ensure it's okay not to return anything here?
+			//return sizeof(reportBuffer);
+			return 0;
+			
+		} else if(rq->bRequest == USBRQ_HID_GET_IDLE){
+			usbMsgPtr = &idleRate;
+			return 1;
+		} else if(rq->bRequest == USBRQ_HID_SET_IDLE){
+			idleRate = rq->wValue.bytes[1];
+		}
+	} else {
+	  /* no vendor specific requests implemented */
+	}
+	return 0;
+}
+
 //static keyboard_data_t reportData;
 
 class UsbKeyboardDevice {	
@@ -415,15 +517,33 @@ class UsbKeyboardDevice {
 	{
 		USBOUT = 0; // TODO: Only for USB pins?
 		USBDDR |= ~USBMASK;
+		//wdt_enable(WDTO_1S);
 
-		usbInit();
+#ifdef _OSCCAL_		
+		uchar   calibrationValue;
+		//Clock calibration setup
+		calibrationValue = eeprom_read_byte(0); /* calibration value from last time */
+		if(calibrationValue != 0xff){
+			OSCCAL = calibrationValue;
+		}		
+#endif _OSCCAL_
 
+		//MCUSR &= ~(1 << WDRF);
+		//wdt_disable(); 	
+//		int i = 0;
+		
 		cli();
 		usbDeviceDisconnect();
-		//_delay_ms(75);	
+		//
 		usbDeviceConnect();
-
+		usbInit();
 		sei();
+		
+		int i = 100;
+		while(--i) {
+			_delay_ms(1);
+			usbPoll();
+		}
 
 		// TODO: Remove the next two lines once we fix
 		//       missing first keystroke bug properly.
@@ -437,7 +557,7 @@ class UsbKeyboardDevice {
 	}
 
 	void delay(){
-		_delay_ms(10);	
+		_delay_ms(1);	 
 	}
 
 	void update() {
@@ -446,7 +566,8 @@ class UsbKeyboardDevice {
 
 	void wait(){		
 		while (!usbInterruptIsReady()) {
-			update();
+			//_delay_us(1);	 
+			usbPoll();
 		}
 	}
 
@@ -536,36 +657,5 @@ class UsbKeyboardDevice {
 
 
 UsbKeyboardDevice UsbKeyboard = UsbKeyboardDevice();
-
-usbMsgLen_t usbFunctionSetup(uchar data[8])
-{
-	usbRequest_t    *rq = (usbRequest_t *)((void *)data);	
-	
-	//static uchar dataBuffer[4];  /* buffer must stay valid when usbFunctionSetup returns */
-
-	if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS)
-	{
-		/* class request type */
-
-		if(rq->bRequest == USBRQ_HID_GET_REPORT)
-		{
-			/* wValue: ReportType (highbyte), ReportID (lowbyte) */
-			//usbMsgPtr = (uchar *)&UsbKeyboard.keyboard_data;
-			/* we only have one report type, so don't look at wValue */ 
-			// TODO: Ensure it's okay not to return anything here?
-			//return sizeof(reportBuffer);
-			return 0;
-			
-		} else if(rq->bRequest == USBRQ_HID_GET_IDLE){
-			usbMsgPtr = &idleRate;
-			return 1;
-		} else if(rq->bRequest == USBRQ_HID_SET_IDLE){
-			idleRate = rq->wValue.bytes[1];
-		}
-	} else {
-	  /* no vendor specific requests implemented */
-	}
-	return 0;
-}
 
 #endif // __UsbKeyboard_h__
