ec0e04438076aa3e67055a3bd963ace860df167f
Zaher Dirkey <zaherdirkey@yahoo.com>
2016-07-11 19:42:20 +0300

    Add: Extract calibrate function to seperated file

--- UsbKeyboard.h
+++ UsbKeyboard.h
@@ -1,16 +1,19 @@
 /*
  * Based on Obdev's AVRUSB code and under the same license.
- *
+ * 
  * TODO: Make a proper file header. :-)
  */
 #ifndef __UsbKeyboard_h__
 #define __UsbKeyboard_h__
-//#define _OSCCAL_
-/*
-	Please Set USB_CFG_HAVE_MEASURE_FRAME_LENGTH to 1 in usbconfig.h
-	
-#define USB_CFG_HAVE_MEASURE_FRAME_LENGTH   1
 
+//#define _OSCCAL_ //Enable it id you want to use internal oscillator
+
+/*
+*
+* Please Set USB_CFG_HAVE_MEASURE_FRAME_LENGTH to 1 in usbconfig.h
+*	
+* #define USB_CFG_HAVE_MEASURE_FRAME_LENGTH   1
+*
 */	
 
 #include <string.h>
@@ -22,6 +25,10 @@
 #include <util/delay.h>     // for _delay_ms() 
 
 #include <avr/pgmspace.h>   // required by usbdrv.h 
+
+#ifdef _OSCCAL_		
+#include <calibrate.h>
+#endif
 
 #ifdef __cplusplus
 extern "C"
@@ -267,7 +274,9 @@ PROGMEM const char usbHidReportDescriptor[USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH] 
 #define KEY_ARROW_LEFT KEY_LEFT //deprecated
  
 //ported from https://github.com/aduitsis/ardumultimedia/blob/master/HID.cpp
+
 #define SHIFT 0x80
+
 PROGMEM const uint8_t asciimap[128] =
 {
 	0x00,             // NUL
@@ -401,70 +410,6 @@ PROGMEM const uint8_t asciimap[128] =
 	0				// DEL
 };
  
-#ifdef _OSCCAL_
-/* ------------------------------------------------------------------------- */
-/* ------------------------ Oscillator Calibration ------------------------- */
-/* ------------------------------------------------------------------------- */
-
-/* Calibrate the RC oscillator to 8.25 MHz. The core clock of 16.5 MHz is
- * derived from the 66 MHz peripheral clock by dividing. Our timing reference
- * is the Start Of Frame signal (a single SE0 bit) available immediately after
- * a USB RESET. We first do a binary search for the OSCCAL value and then
- * optimize this value with a neighboorhod search.
- * This algorithm may also be used to calibrate the RC oscillator directly to
- * 12 MHz (no PLL involved, can therefore be used on almost ALL AVRs), but this
- * is wide outside the spec for the OSCCAL value and the required precision for
- * the 12 MHz clock! Use the RC oscillator calibrated to 12 MHz for
- * experimental purposes only!
- */
-static void calibrateOscillator(void)
-{
-uchar       step = 128;
-uchar       trialValue = 0, optimumValue;
-int         x, optimumDev, targetValue = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
-
-    /* do a binary search: */
-    do{
-        OSCCAL = trialValue + step;
-        x = usbMeasureFrameLength();    /* proportional to current real frequency */
-        if(x < targetValue)             /* frequency still too low */
-            trialValue += step;
-        step >>= 1;
-    }while(step > 0);
-    /* We have a precision of +/- 1 for optimum OSCCAL here */
-    /* now do a neighborhood search for optimum value */
-    optimumValue = trialValue;
-    optimumDev = x; /* this is certainly far away from optimum */
-    for(OSCCAL = trialValue - 1; OSCCAL <= trialValue + 1; OSCCAL++){
-        x = usbMeasureFrameLength() - targetValue;
-        if(x < 0)
-            x = -x;
-        if(x < optimumDev){
-            optimumDev = x;
-            optimumValue = OSCCAL;
-        }
-    }
-    OSCCAL = optimumValue;
-}
-
-/*
-Note: This calibration algorithm may try OSCCAL values of up to 192 even if
-the optimum value is far below 192. It may therefore exceed the allowed clock
-frequency of the CPU in low voltage designs!
-You may replace this search algorithm with any other algorithm you like if
-you have additional constraints such as a maximum CPU clock.
-For version 5.x RC oscillators (those with a split range of 2x128 steps, e.g.
-ATTiny25, ATTiny45, ATTiny85), it may be useful to search for the optimum in
-both regions.
-*/
-
-void usbEventResetReady(void)
-{
-    calibrateOscillator();
-    eeprom_write_byte(0, OSCCAL);   /* store the calibrated value in EEPROM */
-}
-#endif _OSCCAL_
-
 typedef uint8_t byte;
 
 typedef struct 
@@ -510,8 +455,6 @@ usbMsgLen_t usbFunctionSetup(uchar data[8])
 	return 0;
 }
 
-//static keyboard_data_t reportData;
-
 class UsbKeyboardDevice {	
   public:  
 	UsbKeyboardDevice() 
@@ -530,8 +473,7 @@ class UsbKeyboardDevice {
 #endif _OSCCAL_
 
 		//MCUSR &= ~(1 << WDRF);
-		//wdt_disable(); 	
-//		int i = 0;
+    	//wdt_disable(); 	
 		
 		cli();
 		usbDeviceDisconnect();
